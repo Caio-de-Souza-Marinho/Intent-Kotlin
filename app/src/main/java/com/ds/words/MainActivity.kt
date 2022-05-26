d3cbce0bc41234df7f6bc446e926e9e4926b09e7
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-25 22:43:58 -0300

    [Aula 25/05/22]

--- app/src/main/java/com/ds/words/MainActivity.kt
+++ app/src/main/java/com/ds/words/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ds.words
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+
+
+const val EXTRA_MESSAGE = "com.example.words.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+//chamado para quando o usuário apertar o enviar
+fun sendMessage(view: View) {
+    val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+    val message = editText.text.toString()
+    val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+        putExtra(EXTRA_MESSAGE, message)
+    }
+    startActivity(intent)
 }
